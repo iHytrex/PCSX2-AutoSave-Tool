11fc0232e9aba9761da7ba6f97633849964e36b4
Me <Me@DESKTOP-D1S792G>
2022-03-23 21:22:41 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace PCSX2_AUTO_SAVER
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Image = global::PCSX2_AUTO_SAVER.Properties.Resources.gitlogo;
+            this.button1.Location = new System.Drawing.Point(410, 187);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button1.Size = new System.Drawing.Size(62, 62);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(279, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Auto-Save every";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(419, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "minutes.";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(378, 20);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(35, 23);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(29, 22);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(147, 19);
+            this.checkBox3.TabIndex = 11;
+            this.checkBox3.Text = "Use Multiple Save Slots";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(29, 187);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 52);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(182, 187);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(170, 52);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Backup SSTATES";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.ShowIcon = false;
+            this.Text = "PCSX2 AutoSave Tool 1.0.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private NumericUpDown numericUpDown1;
+        private CheckBox checkBox3;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,216 @@
+using System.Diagnostics;
+
+namespace PCSX2_AUTO_SAVER
+{
+    public partial class Form1 : Form
+    {
+        private bool _canCreateNewThread = true;
+        private bool _canExitThread = false;
+        private bool _canStart = true;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = "https://github.com/iHytrex",
+                UseShellExecute = true
+            };
+            Process.Start(psi);
+        }
+
+        // Start/Pause Button: Used for starting or stopping the AutoSave Thread. 
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            // Used for Managing GUI elements.
+            EnableDisable();
+
+            // Check if a new thread can be created.
+            if (_canCreateNewThread)
+            {
+                Console.WriteLine("Can Create new thread!")
+;               _canCreateNewThread = false;
+            }
+            // Responsable for exiting the current function, this way not starting a new thread.
+            else
+            {
+
+                Console.WriteLine("Can't Create new thread!");
+                _canExitThread = true;
+                _canStart = true;
+                _canCreateNewThread = true;
+                return;
+            }
+
+            // AutoSave thread, responsable for all the auto-save features.
+            Thread autoSave = new Thread(() =>
+            {
+                Console.WriteLine("Running!!");
+                while (!_canExitThread)
+                {
+                    if (_canExitThread)
+                    {
+                        Console.WriteLine("Exited While Loop!");
+                        break;
+                    }
+
+                    Console.WriteLine("Sleeping Thread!");
+                    Thread.Sleep((int)numericUpDown1.Value * 3000);
+
+                    if (_canExitThread)
+                    {
+                        Console.WriteLine("Exited While Loop!");
+                        break;
+                    }
+
+                    if (checkBox3.Checked && !_canExitThread)
+                    {
+                        SendKeys.SendWait("{F2}");
+                        Console.WriteLine("Pressed F2!");
+
+                        Thread.Sleep(5000);
+
+                        Console.WriteLine("Exited While Loop!");
+                    }
+
+                    if (!_canExitThread)
+                    {
+                        Console.WriteLine("Pressing F1!");
+                        SendKeys.SendWait("{F1}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Exited While Loop!");
+                    }
+                    
+
+                    if (_canExitThread)
+                    {
+                        Console.WriteLine("Exited While Loop!");
+                        break;
+                    }
+                }
+                Console.WriteLine("Thread finished!");
+            });
+
+            // Starts a new thread.
+            if (_canStart)
+            {
+                _canExitThread = false;
+                _canStart = false;
+                Console.WriteLine("Thread Started!");
+                autoSave.Start();
+            }
+        }
+
+        // Used for Managing GUI elements.
+        private void EnableDisable()
+        {
+            bool status;
+            if (checkBox3.Enabled)
+            {
+                button2.Text = "Stop";
+                status = false;
+            }
+            else
+            {
+                button2.Text = "Start";
+                status = true;
+            }
+
+
+            checkBox3.Enabled = status;
+            numericUpDown1.Enabled = status;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // BETA
+            SaveTools.Backup();
+        }
+    }
+}
+
+public static class SaveTools
+{
+    public static void Backup()
+    {
+        if (!Directory.Exists(@"C:\Users\Me\Documents\PCSX2\sstatesBackups"))
+        {
+            Directory.CreateDirectory(@"C:\Users\Me\Documents\PCSX2\sstatesBackups");
+        }
+
+        DirectoryInfo dI = new DirectoryInfo(@"C:\Users\Me\Documents\PCSX2\sstates");
+        FileInfo[] files = dI.GetFiles("*.P2S");
+
+        foreach (var file in files)
+        {
+            file.CopyTo($"C:\\Users\\Me\\Documents\\PCSX2\\sstatesBackups\\{file.Name}", true);
+        }
+    }
+}
